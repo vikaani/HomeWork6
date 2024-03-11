da27be13890c2b2cada0213483c41d1de283c652
Viktoria <viktoriaani22@gmail.com>
2024-03-11 17:38:23 +0200

    Complete HomeWork6

---
+++ ShopScreen/ShopScreen/ProductDetailsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProductDetailsViewController.swift
+//  ShopScreen
+//
+//  Created by Vika on 08.03.2024.
+//
+
+import UIKit
+
+class ProductDetailsViewController: UIViewController {
+    @IBAction func didTapCreditButton() {
+        print("Credit button tapped")
+    }
+    @IBAction func didTapBuyNowButton() {
+        print("Buy button tapped")
+    }
+    @IBAction func didTapCompareButton() {
+        print("Compare button tapped")
+    }
+    @IBAction func didTapBasketButton() {
+        print("Basket button tapped")
+    }
+    @IBAction func didTapFavoriteButton() {
+        print("Favorite button tapped")
+    }
+}
